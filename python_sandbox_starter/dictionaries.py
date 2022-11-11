3c778cbb2a3bbcb570cab13c737cfb1b7fa5cd6d
Katelyn Miller <katdianemiller@gmail.com>
2022-11-11 17:52:23 -0500

    dicts and tuples

--- python_sandbox_starter/dictionaries.py
+++ python_sandbox_starter/dictionaries.py
@@ -1,2 +1,50 @@
 # A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
 # Read more about dictionaries at https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+# Similar to object literals in JavaScript
+
+#### create
+person = {
+    'first_name': 'Katelyn',
+    'last_name': 'Miller',
+    'age': 25
+}
+
+
+#### create using a constructor
+person2 = dict(first_name='Logan', last_name='Miller')
+
+#### get value
+print(person['first_name'])
+print(person.get('last_name')) # using get method
+
+#### add key/value
+person['phone'] = '111-111-1111'
+
+#### get dict keys
+#print(person.keys())
+
+#### get dict items
+#print(person.items())
+
+#### copy a dict
+person3 = person.copy()
+person3['city'] = 'New York'
+#print(person3)
+
+#### remove an item
+del(person['age'])
+person.pop('phone')
+
+#### clear
+person.clear()
+
+#### get length
+print(len(person3))
+
+#### In javascript, you can have an array of objects. We can also have a list of dictionaries. 
+people = [
+    {'name': 'Jackie', 'age': 50},
+    {'name': 'Eddy', 'age': 54}
+]
+
+print(people)
--- python_sandbox_starter/tuples_sets.py
+++ python_sandbox_starter/tuples_sets.py
@@ -1,5 +1,44 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
  
+#### Create a tuple
+fruits = ('Apples', 'Oranges', 'Grapes')
+
+#### creating a tuple using a constructor
+fruits2 = tuple(('Apples', 'Oranges', 'Grapes'))
+
+#### if you only have one value, you want to leave a trailing comma otherwise, it will looks at it as a string
+fruits3 = ('Apples',)
+#print(fruits3, type(fruits3))
+
+#### get a value
+print(fruits[1])
+
+#### you cannot change the value of an item in a tuple
+#fruits[0] = 'Pears' #cannot do this
+
+#### delete
+del fruits2
+
+#### get length
+print(len(fruits))
 
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+
+#### create a set
+veggies = {'Broccoli', 'Carrot', 'Asparagus'}
+
+#### Check if something is in a set
+print('Broccoli' in veggies)
+
+#### add to set
+veggies.add('Squash')
+
+#### remove from set
+veggies.remove('Carrot')
+
+#### clear set - you still have the veggies set, but nothing is in it.
+veggies.clear()
+
+#### if you want to delete the set entirely:
+del veggies
